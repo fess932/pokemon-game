e7756968327b0d860537a953bb98a6d26ed8ed7d
fess932 <fess932@gmail.com>
2021-01-29 15:05:44 +0300

    homework-3 промежуточное решение

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,25 +1,24 @@
 import s from './style.module.css'
+import * as url from 'url'
 
-const Layout = ({ id, title, descr, urlBg, colorBg }) => {
-  const layoutStyle = {}
-
-  urlBg
-    ? (layoutStyle.backgroundImage = `url(${urlBg})`)
-    : colorBg
-    ? (layoutStyle.background = colorBg)
-    : console.log('пустой фон')
+const Layout = ({ id, title, colorTitle, urlBg, colorBg, children }) => {
+  const sectionStyle = {}
+  if (urlBg) {
+    sectionStyle.backgroundImage = `url(${urlBg})`
+  }
+  if (colorBg) {
+    sectionStyle.backgroundColor = colorBg
+  }
 
   return (
-    <section style={layoutStyle} className={s.root} id={id}>
+    <section style={sectionStyle} className={s.root} id={id}>
       <div className={s.wrapper}>
         <article>
           <div className={s.title}>
-            <h3>{title}</h3>
+            <h3 style={{ color: colorTitle }}>{title}</h3>
             <span className={s.separator} />
           </div>
-          <div className={`${s.desc} ${s.full}`}>
-            <p>{descr}</p>
-          </div>
+          <div className={`${s.desc} ${s.full}`}>{children}</div>
         </article>
       </div>
     </section>
