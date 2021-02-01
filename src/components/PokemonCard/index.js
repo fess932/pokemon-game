0fa28ca35b01b2a9afbb7b2059707e92d696e5d4
fess932 <fess932@gmail.com>
2021-02-01 11:34:12 +0300

    add lib classnames

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,7 +1,9 @@
 import { useState } from 'react'
+import cn from 'classnames'
+
+import cardBackSide from './card-back-side.jpg'
 
 import s from './style.module.css'
-import cardBackSide from './card-back-side.jpg'
 
 const PokemonCard = ({ name, img, id, type, values }) => {
   const [isActive, setActive] = useState(false)
@@ -12,17 +14,17 @@ const PokemonCard = ({ name, img, id, type, values }) => {
 
   return (
     <div
-      className={`${s.pokemonCard} ${isActive ? s.active : ''}`}
+      className={cn(s.pokemonCard, { [s.active]: isActive })}
       onClick={handleClick}
     >
       <div className={s.cardFront}>
-        <div className={`${s.wrap} ${s.front}`}>
-          <div className={`${s.pokemon} ${s[type]}`}>
+        <div className={cn(s.wrap, s.front)}>
+          <div className={cn(s.pokemon, s[type])}>
             <div className={s.values}>
-              <div className={`${s.count} ${s.top}`}>{values.top}</div>
-              <div className={`${s.count} ${s.right}`}>{values.right}</div>
-              <div className={`${s.count} ${s.bottom}`}>{values.bottom}</div>
-              <div className={`${s.count} ${s.left}`}>{values.left}</div>
+              <div className={cn(s.count, s.top)}>{values.top}</div>
+              <div className={cn(s.count, s.right)}>{values.right}</div>
+              <div className={cn(s.count, s.bottom)}>{values.bottom}</div>
+              <div className={cn(s.count, s.left)}>{values.left}</div>
             </div>
             <div className={s.imgContainer}>
               <img src={img} alt={name} />
@@ -39,7 +41,7 @@ const PokemonCard = ({ name, img, id, type, values }) => {
       </div>
 
       <div className={s.cardBack}>
-        <div className={`${s.wrap} ${s.back}`}>
+        <div className={cn(s.wrap, s.back)}>
           <img src={cardBackSide} alt="Card Backed" />
         </div>
       </div>
