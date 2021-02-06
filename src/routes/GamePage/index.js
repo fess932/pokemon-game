8e49da9d42ab641ea5fd3ffba605a5a8563ef75f
fess932 <fess932@gmail.com>
2021-02-06 10:03:56 +0300

    homework-6

--- src/routes/GamePage/index.js
+++ src/routes/GamePage/index.js
@@ -1,20 +1,67 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import GoHome from '../../components/GoHome'
 import PokemonCard from '../../components/PokemonCard'
 
-import POKEMONS from '../../assets/pokemonsData.json'
+import database from '../../service/firebase'
+
 import s from './style.module.css'
 
-const GamePage = () => {
-  const [cards, mutateCards] = useState(POKEMONS)
+function createNewPoke() {
+  return {
+    abilities: ['keen-eye', 'tangled-feet', 'big-pecks'],
+    base_experience: 122,
+    height: 11,
+    id: 17,
+    img:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png',
+    name: 'pidgeotto',
+    stats: {
+      attack: 60,
+      defense: 55,
+      hp: 63,
+      'special-attack': 50,
+      'special-defense': 50,
+      speed: 71,
+    },
+    type: 'flying',
+    values: {
+      bottom: 7,
+      left: 5,
+      right: 2,
+      top: 'A',
+    },
+  }
+}
 
-  const revertPokemon = (id) => {
-    mutateCards((prevState) =>
-      prevState.map((item) =>
-        item.id === id ? { ...item, isActive: !item.isActive } : item
-      )
-    )
+const GamePage = () => {
+  const [cards, mutateCards] = useState({})
+
+  useEffect(() => {
+    database.ref('pokemons').once('value', (snapshot) => {
+      mutateCards(snapshot.val())
+    })
+  }, [cards])
+
+  const addNewPokemonHandle = () => {
+    const newPoke = database.ref('pokemons').push({ ...createNewPoke() })
+    console.log(newPoke.key)
+  }
+
+  const revertPokemon = (uid) => {
+    mutateCards((prevState) => {
+      return Object.entries(prevState).reduce((acc, item) => {
+        const pokemon = { ...item[1] }
+        if (item[0] === uid) {
+          pokemon.isActive = !pokemon.isActive
+          database.ref('pokemons/' + item[0]).set({ ...pokemon })
+        }
+
+        acc[item[0]] = pokemon
+
+        return acc
+      }, {})
+    })
   }
 
   return (
@@ -23,19 +70,26 @@ const GamePage = () => {
 
       <h3 className={s.title}>This is Game Page!</h3>
 
+      <button className={s.btn} onClick={addNewPokemonHandle}>
+        Add new pokemon!
+      </button>
+
       <div className={s.flex}>
-        {cards.map((item) => (
-          <PokemonCard
-            name={item.name}
-            id={item.id}
-            values={item.values}
-            img={item.img}
-            type={item.type}
-            key={item.id}
-            isActive={item.isActive}
-            revertPokemon={revertPokemon}
-          />
-        ))}
+        {Object.entries(cards).map(
+          ([uid, { name, id, values, img, type, isActive }]) => (
+            <PokemonCard
+              name={name}
+              id={id}
+              uid={uid}
+              values={values}
+              img={img}
+              type={type}
+              key={uid}
+              isActive={isActive}
+              revertPokemon={revertPokemon}
+            />
+          )
+        )}
       </div>
     </div>
   )
